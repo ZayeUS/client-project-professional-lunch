8113bdaa45f07f128a67070a4df6561a55ce317c
ZayeUS <ujj.code@gmail.com>
2025-01-21 21:07:03 -0600

    pushed fly.io changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5001;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: 'http://localhost:5173,https://client-project-professional-lunch.fly.dev/', // Frontend URL
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 };
